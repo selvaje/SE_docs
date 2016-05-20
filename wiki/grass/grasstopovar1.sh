8672746157e381cb88a3188eeb8e7a23405c1b8a
Giuseppe Amatulli (lap-top) <Giuseppe.Amatulli@gmail.com>
2016-05-20 15:04:59 -0400

    more push

--- wiki/grass/grasstopovar1.sh
+++ wiki/grass/grasstopovar1.sh
@@ -1,10 +1,11 @@
 
 # get information of the files in the GMTED repository 
 
-gdalinfo  /vsizip/vsicurl/http://edcintl.cr.usgs.gov/downloads/sciweb1/shared/topo/downloads/GMTED/Grid_ZipFiles/md75_grd.zip
+# gdalinfo  /vsizip/vsicurl/http://edcintl.cr.usgs.gov/downloads/sciweb1/shared/topo/downloads/GMTED/Grid_ZipFiles/md75_grd.zip
 
-# create a VRT which the extent of the study region
-gdalbuildvrt -overwrite   -te  5.83333333333333  44.833333333333333333333333  10.83333333333333333333333   48.166666666666666 elevation.vrt  /vsizip/vsicurl/http://edcintl.cr.usgs.gov/downloads/sciweb1/shared/topo/downloads/GMTED/Grid_ZipFiles/md75_grd.zip/md75_grd/w00*x.adf
+echo create a VRT which the extent of the study region
+
+gdalbuildvrt -overwrite  -te  5.83333333333333  44.833333333333333333333333  10.83333333333333333333333   48.166666666666666 elevation.vrt  /vsizip/vsicurl/http://edcintl.cr.usgs.gov/downloads/sciweb1/shared/topo/downloads/GMTED/Grid_ZipFiles/md75_grd.zip/md75_grd/w00*x.adf
 
 gdal_translate -co COMPRESS=DEFLATE -co ZLEVEL=9   elevation.vrt  elevation.tif
 rm elevation.tif
@@ -36,16 +37,3 @@ gdaldem TPI  -co COMPRESS=DEFLATE -co ZLEVEL=9  elevation.tif   tpi.tif
 
 echo  calculate  Roughness   
 gdaldem  roughness   -co COMPRESS=DEFLATE -co ZLEVEL=9 elevation.tif  roughness.tif 
-
-
-
-# start grass 
-
-
-
-rm -rf $OUTDIR/vrm/tiles/loc_$filename
-source /lustre/home/client/fas/sbsc/ga254/scripts/general/create_location.sh $OUTDIR/vrm/tiles  loc_$filename $INDIR/$filename.tif  
-~/.grass7/addons/bin/r.vector.ruggedness.py      elevation=$filename   output=${filename}_vrm  
-r.out.gdal -c  createopt="COMPRESS=LZW,ZLEVEL=9" format=GTiff type=Float64  input=${filename}_vrm  output=$OUTDIR/vrm/tiles/${filename}_"gr.tif" --o
-
-rm -rf $OUTDIR/vrm/tiles/loc_$filename
