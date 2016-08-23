053155c915a01820cc2163c54515aaee415b60db
Stephen Weston <stephen.weston@yale.edu>
2016-08-22 23:11:43 -0400

    Modified to read from file "example.txt".

--- wiki/python/grep_1.py
+++ wiki/python/grep_1.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 #
-# Print all lines from stdin that contain string "argv"
+# Print all lines from "example.txt" that contain the string "line"
 #
 import sys, re
-for s in sys.stdin:
-    if re.search('argv', s):
+for s in open('example.txt', 'rt'):
+    if re.search('line', s):
         sys.stdout.write(s)
