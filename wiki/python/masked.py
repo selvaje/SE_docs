0a3d922c99bf532e596848084105b36f146cebbd
Stephen Weston <stephen.weston@yale.edu>
2016-08-23 21:57:32 -0400

    First attempt.

---
+++ wiki/python/masked.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+import numpy as np
+import numpy.ma as ma
+
+x = np.array([[1, 2, 3], [3, -1, 5]])
+print x.mean()
+
+mask = np.array([[0, 0, 0], [0, 1, 0]])
+mx = ma.masked_array(x, mask=mask)
+print mx.mean()
+print mx
+
+y = np.array([1, 2, 3, 3, 5])
+print y.mean()
+
+print x[x >= 0].mean()
+print x[mask == 0].mean()
