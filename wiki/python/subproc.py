caa26e9d6c7e41a22f726430210fb1ea3656d726
Stephen Weston <stephen.weston@yale.edu>
2016-08-24 10:46:21 -0400

    Added more examples.

--- wiki/python/subproc.py
+++ wiki/python/subproc.py
@@ -2,9 +2,9 @@
 from subprocess import Popen, call, check_output, STDOUT, PIPE
 
 #### bash: ls -l
-status = call(['ls', '-l'])
-
 status = call('ls -l', shell=True)
+
+status = call(['ls', '-l'])
 
 #### bash: s=$(echo $USER)
 s = check_output('echo $USER', shell=True).rstrip()
@@ -19,6 +19,24 @@ s = check_output('echo $USER 1>&2', stderr=STDOUT, shell=True).rstrip()
 print s
 
 #### bash: ls | wc -l
-p = Popen(["ls"], stdout=PIPE)
-status = call(["wc", "-l"], stdin=p.stdout)
+p = Popen(['ls'], stdout=PIPE)
+status = call(['wc', '-l'], stdin=p.stdout)
 status = p.wait()
+
+#### bash: ls | grep '\.sh$' wc -l
+p1 = Popen(['ls'],                              stdout=PIPE)
+p2 = Popen(['grep', '\\.py$'], stdin=p1.stdout, stdout=PIPE)
+p3 = Popen(['wc', '-l'],       stdin=p2.stdout, stdout=PIPE)
+s = p3.communicate()
+print s
+print s[0].strip()
+
+#### bash: ls *.py
+status = call('ls *.py', shell=True)
+
+# this does *not* work since "*.py" is expanded by the shell, not "ls"
+status = call(['ls', '*.py'])
+
+# use glob module to expand "*.py"
+import glob
+status = call(['ls'] + glob.glob('*.py'))
