0d204739733622dbf1da79b2718499bb3ea38127
selvaje74 <selv@pop-os.localdomain>
2022-04-07 20:54:32 +0200

    general pull

---
+++ git_clone_spatialecology_docs.sh
@@ -0,0 +1,6 @@
+cd /homepages/38/d684553722/htdocs/clickandbuilds/spatialecology_docs
+rm -rf  * .git/
+rm -rf  .gitignore
+rm -rf  .gitignore_global
+rm -rf  build  make.bat Makefile requirements.txt source test.txt
+git clone  https://github.com/selvaje/SE_docs.git  .
