b7274b5cd6a26f3c146b6f12b7c9bf9af6825344
selvaje <giuseppe.amatulli@gmail.com>
2025-09-10 08:11:02 +0200

    general pull

--- source/VIRTUALMACHINE/convert_pdf2png.sh
+++ source/VIRTUALMACHINE/convert_pdf2png.sh
@@ -1,4 +1,4 @@
 
-for p in $(seq 0 14) ; do 
-    convert -density 400   -trim  Installation_vm_osgeo-live16.pdf[$p]  -flatten  Installation_vm_osgeo-live16_p$p.png
+for p in $(seq 0 19) ; do 
+    convert -density 400   -trim  Installation_vm_Ubuntu22.04.pdf[$p]  -flatten  Installation_vm_Ubuntu22.04_p$p.png
 done 
